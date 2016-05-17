c7cba6803eb40dc0d0193b3b77b72ff3c0586788
nhsb1 <nhsb1@users.noreply.github.com>
2016-05-16 22:27:51 -0400

    bs4 work #6

--- 52week.py
+++ 52week.py
@@ -4,38 +4,43 @@ import urllib2
 import html5lib
 import time
 from argparse import ArgumentParser 
+import re
 
 url = 'http://www.barchart.com/stocks/high.php?_dtp1=0'
 
 def getSoup():
 	global url, target
 	page = urllib2.urlopen(url)
-	#soup = BeautifulSoup(page, 'html.parser')
-	soup2 = BeautifulSoup(page, 'html.parser')
-	#target = soup.find("table", {"class": "datatable js"}) #good start
-	#target = soup.find_all('tbody') #tbody puts everything in one list[]
-	#target = soup.find_all('a href')
-	#target = soup.find_all('a') #gets all stock, e.g. line '110alpha', but not prices or change
-	target2 = soup2.find_all('tr')
-	#print target
-	#print target
-	#count = 0
-	# for item in target:
-	# 	print str(count) + "alpha", item
-	# 	count = count + 1 
-	#print target[76]
+	soup = BeautifulSoup(page, 'html.parser')
+	target = soup.find_all('tr') #gets all stock, e.g. line '110alpha', but not prices or change
 
 
-# count = 0 
-# mysoup = getSoup()
-# for item in target:
-# 	print str(count) + "alpha", item
-# 	count = count+1
 
-count2 = 0
-for item in target2:
-	print str(count2) + "beta", item
-	count2 = count+1
+		# fullname = clean1.split(",")[0]
+		# lastname = fullname.split(",")[0].split()[-1]
+		#firstname = re.findall(r'([a-zA-Z]\w+)', fullname)[0]
+		#<a href="/quotes/stocks/AEM">AEM</a></td>
+
+count = 0 
+mysoup = getSoup()
+for item in target:
+	stuff = str(item)
+	stuff2 = re.findall(r'(quotes/stocks/\w+)', stuff)
+	if stuff2 is not "":
+		print stuff2
+	
+	
+
+print stuff2
+	#print cleanerstuff
+	#print str(count) + "alpha", item
+
+	#count = count+1
+
+# count2 = 0
+# for item in target2:
+# 	print str(count2) + "beta", item
+# 	count2 = count+1
 #print target
 
 #print mysoup
